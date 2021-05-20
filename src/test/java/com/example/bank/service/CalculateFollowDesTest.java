82d5ba98a87df1831c2af7036d2e01878b498ecf
DoLong <doduclong2208@gmail.com>
2021-05-20 23:53:25 +0700

    add test des

--- src/test/java/com/example/bank/service/CalculateFollowDesTest.java
+++ src/test/java/com/example/bank/service/CalculateFollowDesTest.java
@@ -11,6 +11,7 @@ import javax.transaction.Transactional;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @IntegrationTest
 @Transactional
@@ -43,5 +44,102 @@ public class CalculateFollowDesTest {
         payTest.setPayInformationFollowDecreasing(loanInformation);
         assertEquals(10000000, payTest.getExistence());
     }
+    @Test
+    void tinhNoConLai2() {
+        loanInformation.setLoan(10000000);
+        loanInformation.setLoanPerMonth(loanInformation.getLoan()/loanInformation.getNumOfMonths());
+        payTest.setCountMonth(2);
+        payTest.setPayInformationFollowDecreasing(loanInformation);
+        assertEquals(8333334, payTest.getExistence());
+    }
 
+    @Test
+    void tinhNoConLai3() {
+        loanInformation.setLoan(10000000);
+        loanInformation.setLoanPerMonth(loanInformation.getLoan()/loanInformation.getNumOfMonths());
+        payTest.setCountMonth(-1);
+        payTest.setPayInformationFollowDecreasing(loanInformation);
+        fail("Tháng hiện tại phải lớn hơn 0");
+    }
+
+    @Test
+    void tinhNoConLai4() {
+        loanInformation.setLoan(10000000);
+        loanInformation.setLoanPerMonth(loanInformation.getLoan()/loanInformation.getNumOfMonths());
+        payTest.setCountMonth(12);
+        payTest.setPayInformationFollowDecreasing(loanInformation);
+        assertEquals(0, payTest.getExistence());
+    }
+    @Test
+    void tinhNoConLai5() {
+        loanInformation.setLoan(500000);
+        loanInformation.setLoanPerMonth(loanInformation.getLoan()/loanInformation.getNumOfMonths());
+        payTest.setCountMonth(12);
+        payTest.setPayInformationFollowDecreasing(loanInformation);
+        fail("Số tiền vay phải lớn hơn 1.000.000");
+    }
+    @Test
+    void tinhNoConLai6() {
+        loanInformation.setLoan(100000);
+        loanInformation.setLoanPerMonth(loanInformation.getLoan()/loanInformation.getNumOfMonths());
+        payTest.setCountMonth(13);
+        payTest.setPayInformationFollowDecreasing(loanInformation);
+        assertEquals(0, payTest.getExistence());
+    }
+    @Test
+    void tinhLai1() {
+        loanInformation.setLoan(10000000);
+        loanInformation.setLoanPerMonth(loanInformation.getLoan()/loanInformation.getNumOfMonths());
+        payTest.setCountMonth(0);
+        payTest.setPayInformationFollowDecreasing(loanInformation);
+        assertEquals(31667, payTest.getInterest());
+    }
+    @Test
+    void tinhLai2() {
+        loanInformation.setLoan(10000000);
+        loanInformation.setLoanPerMonth(loanInformation.getLoan()/loanInformation.getNumOfMonths());
+        payTest.setCountMonth(1);
+        payTest.setPayInformationFollowDecreasing(loanInformation);
+        assertEquals(29028, payTest.getInterest());
+    }
+    @Test
+    void tinhLai3() {
+        loanInformation.setLoan(10000000);
+        loanInformation.setLoanPerMonth(loanInformation.getLoan()/loanInformation.getNumOfMonths());
+        payTest.setCountMonth(11);
+        payTest.setPayInformationFollowDecreasing(loanInformation);
+        assertEquals(2639, payTest.getInterest());
+    }
+    @Test
+    void tongTienPhaitra1() {
+        loanInformation.setLoan(10000000);
+        loanInformation.setLoanPerMonth(loanInformation.getLoan()/loanInformation.getNumOfMonths());
+        payTest.setCountMonth(0);
+        payTest.setPayInformationFollowDecreasing(loanInformation);
+        assertEquals(865000, payTest.getTotalPerMonth());
+    }
+    @Test
+    void tongTienPhaitra2() {
+        loanInformation.setLoan(10000000);
+        loanInformation.setLoanPerMonth(loanInformation.getLoan()/loanInformation.getNumOfMonths());
+        payTest.setCountMonth(1);
+        payTest.setPayInformationFollowDecreasing(loanInformation);
+        assertEquals(862361, payTest.getTotalPerMonth());
+    }
+    @Test
+    void tongTienPhaitra3() {
+        loanInformation.setLoan(10000000);
+        loanInformation.setLoanPerMonth(loanInformation.getLoan()/loanInformation.getNumOfMonths());
+        payTest.setCountMonth(11);
+        payTest.setPayInformationFollowDecreasing(loanInformation);
+        assertEquals(835972, payTest.getTotalPerMonth());
+    }
+    @Test
+    void tongTienPhaitra4() {
+        loanInformation.setNumOfMonths(0);
+        payTest.setCountMonth(13);
+        if(loanInformation.getNumOfMonths() < payTest.getCountMonth()){
+            fail("Số tháng vay phải lớn hơn số tháng hiện tại");
+        }
+    }
 }
